bea35c5132c1f399ef9f59e9937ac1047c657715
Jessica Hernandez Botero <jessicahernandez@Jessicas-MacBook.local>
2022-02-02 13:59:10 -0500

    Began js scripting.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 (() => {
-	// set up the puzzle pieces and boards
-	
+	// collect the buttons at the bottom of the page
+	let theThumbnails = document.querySelectorAll("#buttonHolder img"),
+			gameBoard = document.querySelector(".puzzle-board");
+
+	// theThumbnails collects all the image elements in a container
+
+	function changeBgImg() {
+		// debugger;
+		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`
+	}
+
+	// add event handling here
+	theThumbnails.forEach(item => item.addEventListener("click", changeBgImg));
+
 })();
